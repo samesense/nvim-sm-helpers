dc02a58ad6876495df73688ea3206a687738f9b4
Perry <samesense@users.noreply.github.com>
2024-04-28 19:02:27 -0400

    Update module.lua

--- lua/plugin_name/module.lua
+++ lua/plugin_name/module.lua
@@ -1,9 +1,16 @@
 ---@class CustomModule
 local M = {}
 
----@return string
-M.my_first_function = function(greeting)
-  return greeting
+---@return nil
+M.open_and_insert = function(greeting)
+  -- Open the file run.sh                                                                                 
+  vim.cmd('edit run.sh')                                                                                  
+                                                                                                            
+  -- Ensure the buffer is loaded                                                                          
+  vim.api.nvim_buf_set_lines(0, 3, 3, false, {"--forcerun doit"})                                         
+                                                                                                            
+  -- Optionally, save the file after the modification                                                     
+  vim.cmd('write') 
 end
 
 return M
